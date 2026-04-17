c2186a519be0e155e28b531fd80cce1e28e6a544
Sarah A <saraharguetaluv@gmail.com>
2026-04-17 16:19:31 -0400

    tailwind and lucide installed

---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+import '../styles/SearchBar.css'
+import { Search } from 'lucide-react';
+import { useState, useCallback, useEffect } from 'react';
+
+const SearchBar = () => {
+
+    return(
+        <>
+        </>    
+    )
+    
+}
+
+export default SearchBar;
