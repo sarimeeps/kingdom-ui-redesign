59825bad236a8ef91b65d9fd45559d1974adb1a7
Sarah A <saraharguetaluv@gmail.com>
2026-05-07 12:50:57 -0400

    adjusted navbar and added shop dropdown

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,59 +4,92 @@ import SearchBar from './SearchBar';
 import { Menu } from 'lucide-react';
 import { useState } from 'react';
 
-function Navbar(){
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+
+function Navbar() {
 
     const navItems = [
-        {id: 1, text: 'Home', route: '/'},
-        {id: 2, text: 'Shop', route: '/'},
-        {id: 3, text: 'Karaoke', route: '/'},
-        {id: 4, text: 'Events', route: '/'},
-        {id: 5, text: 'About Us', route: '/'},
-        {id: 6, text: 'FAQ', route: '/'},
+        { id: 1, text: 'Home', route: '/' },
+        { id: 2, text: 'Shop', route: '/' },
+        { id: 3, text: 'Karaoke', route: '/' },
+        { id: 4, text: 'Events', route: '/' },
+        { id: 5, text: 'About Us', route: '/' },
+        { id: 6, text: 'FAQ', route: '/' },
+    ]
+
+    const shopItems = [
+        { id: 1, text: 'K-Pop', route: '/' },
+        { id: 2, text: 'Bake Shop', route: '/' },
+        { id: 3, text: 'Book Shop', route: '/' }
     ]
 
     const [isOpen, setIsOpen] = useState(false);
+    const [dropOpen, setDropOpen] = useState(false);
 
     const handleMenu = () => {
         setIsOpen(!isOpen);
     }
 
-    return(
+    return (
         <header className='relative mx-auto w-full px-2 py-4'>
             <nav className="max-w-[1920px] mx-auto flex items-center justify-between bg-white border border-[#fff] rounded-xl py-6 px-4 md:px-6 lg:px-8">
                 {/* mobile nav icon */}
                 <div className="lg:hidden cols-1 cursor-pointer" onClick={ handleMenu }>
-                    <Menu size={27}/>
+                    <Menu size={27} />
                 </div>
-                
-                <div id="logo" className='absolute left-1/2 -translate-x-1/2 flex-shrink-0 h-12  lg:static lg:translate-x-0'>
+
+                <div id="logo" className='absolute left-1/2 -translate-x-1/2 flex-shrink-0 h-13  lg:static lg:translate-x-0'>
                     <a href="/">
-                        <img src={ logo } alt="kingdom-logo" className='h-full object-contain'/>
+                        <img src={logo} alt="kingdom-logo" className='h-full object-contain' />
                     </a>
                 </div>
 
                 {/* desktop nav links */}
-                <ul className='hidden lg:flex flex-1 justify-center gap-1 text-sm md:text-base whitespace-nowrap rounded-xl'>
-                    {navItems.map(item => (
-                        <li
-                        key={item.id}
-                        className='p-1 text-[var(--text-h)] text-lg rounded-xl m-2 cursor-pointer whitespace-nowrap'>
-                            {item.text}
-                        </li>
-                    ))}
+                <ul className='hidden lg:flex flex-1 justify-center gap-8 whitespace-nowrap rounded-xl text-[var(--text-h)] text-xl p-1 m-2'>
+                    <li className='p-1 cursor-pointer'>Home</li>
+
+                    <div className='flex flex-col relative' onMouseEnter={() => setDropOpen(true) } onMouseLeave={() => setDropOpen(false)}>
+                        <div className="group flex flex-row items-center cursor-pointer">
+                            <li className='p-1 cursor-pointer'>Shop</li>
+                            <IoIosArrowDown className={`${ dropOpen ? 'rotate-180' : 'rotate-0'} transition duration-300`}/>
+                        </div>
+                        <div className=''>
+                            {/* shop dropdown menu */}
+                        {
+                            dropOpen && (
+                                <ul className={`absolute top-9 border border-[#E193CD] rounded-lg bg-[#fff] transition-transform duration-300 ease-in-out `}>
+                                    {shopItems.map(item => (
+                                        <li
+                                            key={item.id}
+                                            className='cursor-pointer hover:bg-[var(--text-h)] hover:text-[#fff] rounded-sm'
+                                        >
+                                            <div className='p-4 hover:translate-x-1 transition-transform duration-200'><p>{item.text}</p></div>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )
+                        }
+                        </div>
+                    </div>
+                    <li className='p-1 cursor-pointer'>Karaoke</li>
+                    <li className='p-1 cursor-pointer'>Events</li>
+                    <li className='p-1 cursor-pointer'>About Us</li>
+                    <li className='p-1 cursor-pointer'>FAQ</li>
                 </ul>
                 <div id='search-container' className='hidden md:block w-auto max-w-[140px] flex justify-end lg:max-w-50'>
                     <SearchBar />
                 </div>
             </nav>
-                {/* mobile nav menu */}
+
+
+            {/* mobile nav menu */}
             {
                 isOpen && (
                     <ul className="lg:hidden md:grid-cols-3 md:grid-rows-2 absolute top-10/12 left-2 right-2 border-[#fff] rounded bg-white grid grid-cols-1 items-start gap-6 ">
                         {navItems.map(item => (
                             <li
-                            key={item.id}
-                            className='p-2 hover:bg-[var(--text-h)] hover:text-white rounded-xl cursor-pointer'>
+                                key={item.id}
+                                className='p-2 hover:bg-[var(--text-h)] hover:text-white rounded-xl cursor-pointer'>
                                 {item.text}
                             </li>
                         ))}
--- client/src/components/RouteCard.jsx
+++ client/src/components/RouteCard.jsx
@@ -5,11 +5,11 @@ const RouteCard = ({ image, title, link }) => {
     return (
         <Link to={link}>
             <div className="group relative w-full aspect-[3/1] md:aspect-[4/3] overflow-hidden rounded-lg bg-white">
-                <img src={image} alt="route-card" className='absolute inset-0 w-full h-full object-cover group-hover:scale-110 transition duration-300' />
+                <img src={image} alt={title} className='absolute w-full h-full object-cover group-hover:scale-110 transition duration-300' />
                 <div className="absolute bg-[#475B76]/40 group-hover:bg-[#475B76]/50 transition duration-300 w-full h-full"></div>
                 <div className="absolute inset-0 flex items-center justify-center">
-                    <span class="bg-left-bottom bg-gradient-to-r from-[#fff] to-[#fff] bg-[length:0%_4px] bg-no-repeat group-hover:bg-[length:100%_4px] transition-all duration-500 ease-out">
-                        <h2 id='card-title' className='text-white group-hover:-translate-y-1 transition-all duration-300'>{title}</h2>
+                    <span className="bg-left-bottom bg-gradient-to-r from-[#fff] to-[#fff] bg-[length:0%_4px] bg-no-repeat group-hover:bg-[length:100%_4px] transition-all duration-300 ease-out">
+                        <h2 id='card-title' className='text-white group-hover:-translate-y-1 transition duration-300'>{title}</h2>
                     </span>
                 </div>
             </div>
