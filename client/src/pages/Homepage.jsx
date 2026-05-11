0967a21726d130aa1db8ea921eccc84309df4eb0
Sarah A <saraharguetaluv@gmail.com>
2026-05-11 16:38:49 -0400

    corrected homepage elements sizing and layout issues

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -37,40 +37,40 @@ const Homepage = () => {
             </ul>
 
             {/* About Section */}
-            <section className="w-full justify-items-center mt-16 p-20 bg-linear-to-l from-[#E79CD6]/50 to-[#E7AAD4]/50 to-[#E6ABC9]/50 to-[#E8B7D2]/50 to-[#F4C6D3]/50">
-                <div className="grid lg:grid-cols-2 max-w-[1920px] items-center space-x-10">
-                    <div className="max-w-2xl mb-12">
+            <section className="w-full justify-items-center mt-20 p-20 bg-linear-to-l from-[#E79CD6]/50 to-[#E7AAD4]/50 to-[#E6ABC9]/50 to-[#E8B7D2]/50 to-[#F4C6D3]/50">
+                <div className="grid lg:grid-cols-2 max-w-[1920px] gap-10">
+                    <div className="max-w-2xl w-full mx-auto mb-12">
                         <h1 className='lg:text-left text-center my-6 lg:mt-6'>Happy You're Here!</h1>
                         <hr class="hidden lg:block w-12 h-1 mb-9 mt-2 bg-[var(--text-h)] "></hr>
-                        <p className='font-(family-name:--sans) lg:text-[1.375rem] md:text-xl text-lg'>
+                        <p className='font-(family-name:--sans) text-center lg:text-left lg:text-[1.375rem] md:text-xl text-lg'>
                             Welcome to Kingdom of K-pop, Romance Books, and Bake Shop! We are a small, woman-owned family business dedicated to creating a welcoming space for North Carolina’s K-pop fans, romance book lovers, and dessert enthusiasts.
                         </p>
-                        <p className='font-(family-name:--sans) lg:text-[1.375rem] md:text-xl text-lg'>
+                        <p className='font-(family-name:--sans) text-center lg:text-left lg:text-[1.375rem] md:text-xl text-lg'>
                             Our goal is to build a fun and supportive community where you can celebrate your favorite artists, authors, and more. Our physical store opened in <strong>Clemmons, NC</strong> in July 2024, and we’re excited to grow together with your support!
                         </p>
                         <Link to='/about'>
-                        <button id='cta' className='flex items-center gap-4 mt-6'>
+                            <button id='cta' className='flex mx-auto lg:mx-0 items-center gap-4 mt-6'>
                                 READ MORE
                                 <IoArrowForwardCircleOutline size={25} />
                             </button>
                         </Link>
-                </div>
-                    <figure className="min-w-min p-0 justify-items-end">
+                    </div>
+                    <figure className="min-w-min p-0 justify-items-center lg:justify-items-end">
                         <img src={front} alt="Kingdom's storefront" />
                     </figure>
                 </div>
             </section>
             {/* Socials Section */}
-            <section className="max-w-[1920px] w-full gap-40 grid lg:grid-cols-2 pt-20 mt-20 -mb-20">
-                <article className='lg:justify-self-end'>
+            <section className="max-w-[1920px] w-full gap-20 lg:gap-40 grid lg:grid-cols-2 pt-4 md:mt-40 mb-16 lg:-mb-20">
+                <article className='hidden lg:block lg:justify-self-end'>
                     <InstaPlugin />
                 </article>
-                <div className="flex flex-col gap-6 md:ml-6 lg:text-left text-center">
+                <div className="flex flex-col gap-6 lg:ml-6 lg:text-left text-center mx-auto lg:mx-0">
                     <h1 className='mt-6'>Follow Our Socials!</h1>
-                    <hr class="hidden md:block w-12 h-1 mb-6 bg-[var(--text-h)] "></hr>
-                    <p className='w-md font-(family-name:--sans) text-xl font-medium justify-center items-center'>Catch up on new arrivals and restock as we post updates on social media!</p>
-                    <ul className='flex flex-col gap-6 items-center md:items-start justify-center font-(family-name:--btn-font)'>
-                        <li className=' w-md rounded-2xl p-2 pl-4 bg-gradient-to-r from-[#ffd600] via-[#ff7a00] via-2% via-[#ff0069] via-8% via-[#e90097] via-11% to-[#7638fa] to-79%'>
+                    <hr class="hidden lg:block w-12 h-1 mb-6 bg-[var(--text-h)] "></hr>
+                    <p className='lg:w-md font-(family-name:--sans) text-xl font-medium justify-center items-center'>Catch up on new arrivals and restock as we post updates on social media!</p>
+                    <ul className='flex flex-col gap-6 mt-4 items-center lg:items-start justify-center font-(family-name:--btn-font)'>
+                        <li className='w-sm md:w-md rounded-2xl p-2 pl-4 bg-gradient-to-r from-[#ffd600] via-[#ff7a00] via-2% via-[#ff0069] via-8% via-[#e90097] via-11% to-[#7638fa] to-79%'>
                             <a href="https://www.instagram.com/kingdomofkpopandmore/"
                                 target="_blank"
                                 rel="noopener noreferrer"
@@ -84,7 +84,7 @@ const Homepage = () => {
                                 </div>
                             </a>
                         </li>
-                        <li className='w-md rounded-2xl bg-[#1E6ED5] p-2 pl-4'>
+                        <li className='w-sm md:w-md rounded-2xl bg-[#1E6ED5] p-2 pl-4'>
                             <a
                                 href="https://www.facebook.com/profile.php?id=61558391370752"
                                 target="_blank"
@@ -101,7 +101,7 @@ const Homepage = () => {
 
                             </a>
                         </li>
-                        <li className='w-md rounded-2xl bg-[#000000] p-2 pl-4'>
+                        <li className='w-sm md:w-md rounded-2xl bg-[#000000] p-2 pl-4'>
                             <a
                                 href="https://www.tiktok.com/@kingdomkpopandmore"
                                 target="_blank"
