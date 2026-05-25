271926a0056128b20f986ca4f3d050b330baa87b
Sarah A <saraharguetaluv@gmail.com>
2026-05-25 11:48:32 -0400

    faq page start

--- client/src/pages/Faq.jsx
+++ client/src/pages/Faq.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const Faq = () => {
+
   return (
-    <div>Faq</div>
+    <div className='w-full flex flex-col items-center justify-center'>
+      <div className='w-full'>
+      <h1 className='align-self-start'>Ask Us Anything!</h1>
+      </div>
+    </div>
   )
 }
 
