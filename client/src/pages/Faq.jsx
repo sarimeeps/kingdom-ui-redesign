ed69323bb6e5a816793a5e7cf2f719ea607dd08b
Sarah A <saraharguetaluv@gmail.com>
2026-05-25 17:04:42 -0400

    form box color adjust

--- client/src/pages/Faq.jsx
+++ client/src/pages/Faq.jsx
@@ -13,19 +13,19 @@ const Faq = () => {
           <form action="" className='grid grid-cols-2 gap-5 max-w-150 mt-6' id='query'>
             <label htmlFor="name">
               <span className='sr-only'>Name</span>
-              <input type="text" id='name' name='name' placeholder='Name' className='border border-gray-400 rounded-sm p-2 w-full focus:border-[#8F3A6D] focus:outline-none focus:ring-1 focus:ring-[#8F3A6D] shadow-xs' />
+              <input type="text" id='name' name='name' placeholder='Name' className='bg-[#fff] border border-gray-400 rounded-sm p-2 w-full focus:border-[#8F3A6D] focus:outline-none focus:ring-1 focus:ring-[#8F3A6D] shadow-xs' />
             </label>
             <label htmlFor="email">
               <span className='sr-only'>Email</span>
-              <input type="email" id='email' name='email' placeholder='Email' className='border border-gray-400 rounded-sm p-2 w-full focus:border-[#8F3A6D] focus:outline-none focus:ring-1 focus:ring-[#8F3A6D] shadow-xs' />
+              <input type="email" id='email' name='email' placeholder='Email' className='bg-[#fff] border border-gray-400 rounded-sm p-2 w-full focus:border-[#8F3A6D] focus:outline-none focus:ring-1 focus:ring-[#8F3A6D] shadow-xs' />
             </label>
             <label htmlFor="tel" className='col-span-2'>
               <span className='sr-only'>Phone Number</span>
-              <input type="tel" id='tel' name='tel' pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}' placeholder='Phone Number' className='border border-gray-400 p-2 rounded-sm w-full focus:border-[#8F3A6D] focus:outline-none focus:ring-1 focus:ring-[#8F3A6D] shadow-xs' />
+              <input type="tel" id='tel' name='tel' pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}' placeholder='Phone Number' className='bg-[#fff] border border-gray-400 p-2 rounded-sm w-full focus:border-[#8F3A6D] focus:outline-none focus:ring-1 focus:ring-[#8F3A6D] shadow-xs' />
             </label>
             <label htmlFor="query" className='col-span-2'>
               <span className='sr-only'>Question</span>
-              <textarea id='query' name='query' placeholder='Question *' className='border border-gray-400 p-2 rounded-sm w-full h-30 focus:border-[#8F3A6D] focus:outline-none focus:ring-1 focus:ring-[#8F3A6D] shadow-xs' required />
+              <textarea id='query' name='query' placeholder='Question *' className='bg-[#fff] border border-gray-400 p-2 rounded-sm w-full h-30 focus:border-[#8F3A6D] focus:outline-none focus:ring-1 focus:ring-[#8F3A6D] shadow-xs' required />
             </label>
             <button type='submit' className='w-fit flex mx-auto lg:mx-0 items-center text-[#ffffff] font-(family-name:--btn-font) lg:text-base md:text-sm font-bold bg-[var(--button)] border-2 border-white rounded-3xl px-9 py-2 active:bg-[var(--text-h)] active:font-extrabold'>SUBMIT</button>
           </form>
