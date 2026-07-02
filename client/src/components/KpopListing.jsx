fb666b2a5de598119b2a2314af57a06ce689493d
seenapatel <seenabpatel@gmail.com>
2026-07-02 16:14:19 -0400

    added listings and filters to the kpop page

--- client/src/components/KpopListing.jsx
+++ client/src/components/KpopListing.jsx
@@ -1,20 +1,18 @@
 
-const KpopListing = ([ image, artist, album, price]) => {
+const KpopListing = ({ title, group, price, image, status }) => {
     return (
-        <div>
+        <div className="max-w-90 h-full mb-5">
             <img 
                 src={image}
-                alt={album}
-                className="aspect-square m-10 object-cover overflow-hidden"
+                alt={image}
+                className="aspect-square drop-shadow-sm object-cover overflow-hidden mb-4"
             />
+            <p className='text-base text-[var(--text)] truncate w-90'>{ group }</p>
+                <p className='font-semibold text-[var(--text)] text-[1.375rem] truncate w-90'>{ title }</p>
             <p>
-                {artist}
-            </p>
-            <p>
-                {album}
-            </p>
-            <p>
-                ${price.toFixed(2)}
+               {
+                    status === true ? <p className='font-medium text-lg text-[var(--text)]'>${ price }</p> : <p className="font-medium text-lg text-[#D81313]">Out Of Stock</p>
+                }
             </p>
         </div>
     )
